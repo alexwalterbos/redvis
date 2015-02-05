b7835496e791cbf2c75aaf27a78ead079c7f7839
Alex Walterbos <atw.231@gmail.com>
2015-02-05 15:45:52 +0100

    Added smoothness

--- visualization-logic.js
+++ visualization-logic.js
@@ -1,15 +1,18 @@
 function draw() {
+	// Load data
 	var request = new XMLHttpRequest();
 	request.open("GET", "graph.json", false);
 	request.send(null)
-
 	var data = JSON.parse(request.responseText);
-	console.log(data);
 	
 	// Instantiate our network object.
 	var container = document.getElementById('subreddits');
 
 	var options = {
+		smoothCurves: {
+			type: 'straightCross',
+			roundness: 0
+		},
 		nodes: {
 		  shape: 'dot'
 		},
